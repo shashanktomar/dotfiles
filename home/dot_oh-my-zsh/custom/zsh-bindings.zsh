e45e528ba30d67bbfe81806f7784bb10fc13be16
Shashank Tomar <shashank004@gmail.com>
2024-12-03 15:07:58 +1100

    update

--- home/dot_oh-my-zsh/custom/zsh-bindings.zsh
+++ home/dot_oh-my-zsh/custom/zsh-bindings.zsh
@@ -8,3 +8,10 @@ ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLOCK
 ZVM_OPPEND_MODE_CURSOR=$ZVM_CURSOR_UNDERLINE
 
 ZVM_VI_INSERT_ESCAPE_BINDKEY=jj
+
+############## ZSH Autocomplete plugin keybindings ###########
+# bindkey '^I' menu-select
+# bindkey "$terminfo[kcbt]" reverse-menu-select
+
+bindkey -M menuselect              '^I'         menu-complete
+bindkey -M menuselect "$terminfo[kcbt]" reverse-menu-complete
