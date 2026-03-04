b8af8eedc62ab58a645f8bc99f4fadce5d5b40b1
Shashank Tomar <shashank004@gmail.com>
2026-03-04 16:46:07 +1100

    update

--- home/dot_oh-my-zsh/custom/pkg_core.zsh
+++ home/dot_oh-my-zsh/custom/pkg_core.zsh
@@ -13,5 +13,14 @@ export OP_BIOMETRIC_UNLOCK_ENABLED=true
 # claude code
 export CLAUDE_CONFIG_DIR=~/.config/claude
 
+# yazi - change cwd when exiting
+y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  command yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd < "$tmp"
+  [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+  rm -f -- "$tmp"
+}
+
 # Required for shell integration in vscode to work properly
 [[ "$TERM_PROGRAM" == "vscode" ]] && . "$(code --locate-shell-integration-path zsh)"
--- home/dot_oh-my-zsh/custom/pkg_utils.zsh
+++ home/dot_oh-my-zsh/custom/pkg_utils.zsh
@@ -2,9 +2,10 @@ export PATH=$PATH:~/.docker/bin
 
 ########## Home Assistant ##########
 export HASS_SERVER=http://home-assistant.local:8123
+export HA_SSH_HOST=ha@192.168.4.29
 # Dont forget to export HASS_TOKEN=<secret>
 
 source <(_HASS_CLI_COMPLETE=zsh_source hass-cli)
 
 alias hac="hass-cli"
-alias ssh-ha="ssh ha@192.168.4.29"
+alias ssh-ha="ssh $HA_SSH_HOST"
